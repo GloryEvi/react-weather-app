f061d74ddeda155296d4b47e6ade13ac19c56709
GloryEvi <167655575+GloryEvi@users.noreply.github.com>
2024-06-15 17:48:45 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,29 +3,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/GloryEvi" target="_blank" rel="noreferrer">
-          Glory Evi
-        </a>{" "}
-        open-sourced on{" "}
-        <a
-          href="https://github.com/GloryEvi/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://e-react-weather-app.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/GloryEvi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Glory Evi
+          </a>{" "}
+          open-sourced on{" "}
+          <a
+            href="https://github.com/GloryEvi/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://e-react-weather-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
