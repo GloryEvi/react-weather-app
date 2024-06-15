b035318e8653801159b084adf0276a3c8b790d78
GloryEvi <167655575+GloryEvi@users.noreply.github.com>
2024-06-15 02:12:44 +0100

    Added github repo url and netlify link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/GloryEvi" target="_blank" rel="noreferrer">
+          Glory Evi
+        </a>{" "}
+        open-sourced on{" "}
+        <a
+          href="https://github.com/GloryEvi/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://e-react-weather-app.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
