7f9d837d504794900e1916c10c2e16c566f8c5c4
GloryEvi <167655575+GloryEvi@users.noreply.github.com>
2024-06-19 01:26:44 +0100

    Connected the forecast API call to the layout

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,33 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="forecast-day">{day()}</div>
+      <WeatherIcon code={props.data.weather[0].icon} size={40} />
+      <div className="weatherForecastTemperatures">
+        <span className="max-temperature">{maxTemperature()}</span>
+        <span className="min-temperature">{minTemperature()}</span>
+      </div>
+    </div>
+  );
+}
